12bdf4457b4f6fc44a708e60f13fbf174817b00e
Changpeng Liu <changpeng.liu@intel.com>
2018-08-27 21:06:33 -0400

    Add SPDK BlobFS integration.
    This patches adds support for using the SPDK BlobFS as the
    storage engine for a RocksDB database.
    
    The Makefile changes assume that SPDK and DPDK have been
    built according to the SPDK README (https://github/spdk/spdk)
    and share the same parent directory as the RocksDB repository.
    
    Flags to configure the SPDK storage engine:
    
    spdk -
      Defines the name of the SPDK configuration file.  If
      omitted, will use PosixEnv instead of SpdkEnv.  This
      configuration file defines the block devices that may
      contain the BlobFS for the database.
    
    spdk_bdev -
      Defines the name of the SPDK block device which
      contains the BlobFS for this database.
    
    spdk_cache_size -
      Defines the amount of userspace cache memory used by
      BlobFS.  Specified in terms of megabytes (MB).  Default
      is 4096 (4GB).
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Signed-off-by: Ben Walker <benjamin.walker@intel.com>
    Signed-off-by: Maciej Szwed <maciej.szwed@intel.com>
    Reviewed-on: https://review.gerrithub.io/423596 (spdk-v5.14.3 branch)
    Change-Id: I3f0c11e2bb69913ef1318f4eb6b8dc339b095306
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/rocksdb/+/6485
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

--- src.mk
+++ src.mk
@@ -88,6 +88,7 @@ LIB_SOURCES =                                                   \
   env/env_encryption.cc                                         \
   env/env_hdfs.cc                                               \
   env/env_posix.cc                                              \
+  $(SPDK_DIR)/lib/rocksdb/env_spdk.cc                      \
   env/file_system.cc                                            \
   env/fs_posix.cc                                               \
   env/fs_remap.cc                                               \
